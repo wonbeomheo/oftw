6b90f11d6ad59909678692fd0fc0fba3e211ba1f
Won Beom Heo <wonbeom.heo@classact.co.kr>
2024-03-27 14:15:56 +0900

    fix: add a selectCity trigger
    Send a request to the openweathermap API only once when the page is loaded

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,33 +1,33 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Weather = () => {
     const [res, setRes] = useState({});
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [city,setCity] = useState("Seoul");
 
     const API_KEY = import.meta.env.VITE_OPEN_WEATHER;
 
     const selectCity = () => {
         fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=${API_KEY}&lang=en`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}&lang=en`
         )
+            .then((response) => response.json())
             .then((response) => {
-                setIsLoaded(() => {return true});
-                response.json();
-            })
-            .then((response) => {
-                setRes({...response});
+                let data = {...response};
+                setRes(data);
             });
     };
 
-    if(!isLoaded) {
+    useEffect(() => {
         selectCity();
-    }
+    }, [city]);
 
     return <>
         <div className="flex pr-6 sm:pr-10">
             <div className="flex flex-col">
-                <span id="weather__temp_max" className="text-sm sm:text-base text-right">{Object.keys(res).includes('main') ? `${res.main.temp_max} °C` : null}</span>
-                <span id="weather__temp_min" className="text-sm sm:text-base text-right">{Object.keys(res).includes('main') ? `${res.main.temp_min} °C` : null}</span>
+                <span id="weather__temp_max"
+                      className="text-sm sm:text-base text-right">{Object.keys(res).includes('main') ? `${res.main.temp_max} °C` : null}</span>
+                <span id="weather__temp_min"
+                      className="text-sm sm:text-base text-right">{Object.keys(res).includes('main') ? `${res.main.temp_min} °C` : null}</span>
             </div>
             <div id="weather__icon">
                 <img
